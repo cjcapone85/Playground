99552c4686abb4ee88e944c48ae0756380b5c850
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-03-17 16:56:40 -0500

    trying out TS

---
+++ basics.ts
@@ -0,0 +1,26 @@
+let age: number;
+
+age = 12;
+
+let username: string;
+
+username = 'Chad';
+
+let isInstructor: boolean;
+
+isInstructor = false;
+
+let hobbies: string[];
+
+hobbies = ['sports', 'hunting', 'cooking'];
+
+let person: {};
+
+person = {
+    name: 'Chad',
+    age: 37
+};
+
+person = {
+    isEmployee: true
+};
